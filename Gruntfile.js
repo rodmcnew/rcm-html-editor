e48677262a5305d249c8dd09d2982f1e99574c57
James Jervis <jjervis@relivinc.com>
2015-06-09 14:49:02 -0500

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig(
+        {
+            pkg: grunt.file.readJSON('package.json'),
+            uglify: {
+                dist : {
+                    options: {
+                        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                        mangle: false,
+                        sourceMap: true,
+                        sourceMapName: 'dist/<%= pkg.name %>.map'
+                    },
+                    files: {
+                        'dist/<%= pkg.name %>.min.js': [
+                            'src/adapter-tinymce/rcm-html-editor-config.js',
+                            'src/adapter-tinymce/rcm-html-editor-options.js',
+                            'src/adapter-tinymce/rcm-html-editor.js',
+                            'src/adapter-tinymce/rcm-html-editor-toolbar.js',
+                            'src/rcm-html-editor-guid.js',
+                            'src/rcm-html-editor-event-manager.js',
+                            'src/rcm-html-editor-service.js',
+                            'src/angular-rcm-html-editor.js'
+                        ],
+                        'dist/<%= pkg.name %>.min.css': ['src/adapter-tinymce/css/editor.css']
+                    }
+                }
+            },
+            copy: {
+                dist: {
+                    expand: true,
+                    cwd: 'src/adapter-tinymce/html',
+                    src: '**',
+                    dest: 'dist/html'
+                }
+            },
+            concat: {
+                options: {
+                },
+                dist: {
+                    src: [
+                        'src/adapter-tinymce/rcm-html-editor-config.js',
+                        'src/adapter-tinymce/rcm-html-editor-options.js',
+                        'src/adapter-tinymce/rcm-html-editor.js',
+                        'src/adapter-tinymce/rcm-html-editor-toolbar.js',
+                        'src/rcm-html-editor-guid.js',
+                        'src/rcm-html-editor-event-manager.js',
+                        'src/rcm-html-editor-service.js',
+                        'src/angular-rcm-html-editor.js'
+                    ],
+                    dest: 'dist/<%= pkg.name %>.js'
+                }
+            }
+        }
+    );
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'copy', 'concat']);
+};
